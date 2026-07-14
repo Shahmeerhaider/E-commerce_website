0f233e5644cb659ed3791d568d057d5cc248c0d1
Shahmeerhaider <142961129+Shahmeerhaider@users.noreply.github.com>
2026-07-14 14:59:01 +0500

    Delete sam-collection directory

--- sam-collection/backend/routes/paymentRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/paymentRoutes.js
-const express = require('express');
-const router = express.Router();
-const { createPaymentIntent } = require('../controllers/paymentController');
-const { protect } = require('../middleware/auth');
-
-router.post('/create-intent', protect, createPaymentIntent);
-
-module.exports = router;
